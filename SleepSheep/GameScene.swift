cf48b0bf469053e4d469f6dfafbd21204013a5bb
海野恵凜那 <erina@unnomegumirinna-no-macbook-pro.local>
2017-02-03 20:51:26 +0900

    コード部分完了。

---
+++ SleepSheep/GameScene.swift
@@ -0,0 +1,127 @@
+//
+//  GameScene.swift
+//  SleepSheep
+//
+//  Created by 海野恵凜那 on 2017/02/01.
+//  Copyright © 2017年 erina.umino. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var scrollNode:SKNode!
+    var sheep:SKSpriteNode!
+
+    // SKView上にシーンが表示されたときに呼ばれるメソッド
+    override func didMove(to view: SKView) {
+        // 背景色を設定
+        //backgroundColor = UIColor(colorLiteralRed: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        // スクロールするスプライトの親ノード
+        scrollNode = SKNode()
+        addChild(scrollNode)
+        
+        setupGround()
+        setupCloud()
+    }
+    
+    func setupGround() {
+        // 地面の画像を読み込む
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = SKTextureFilteringMode.nearest
+        
+        // 必要な枚数を計算
+        let needNumber = 2.0 + (frame.size.width / groundTexture.size().width)
+        
+        // スクロールするアクションを作成
+        // 左方向に画像一枚分スクロールさせるアクション
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width , y: 0, duration: 5.0)
+        
+        // 元の位置に戻すアクション
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0.0)
+        
+        // 左にスクロール->元の位置->左にスクロールと無限に繰り替えるアクション
+        let repeatScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
+        
+        // スプライトを配置する
+        stride(from: 0.0, to: needNumber, by: 1.0).forEach { i in
+            let sprite = SKSpriteNode(texture: groundTexture)
+            
+            // スプライトの表示する位置を指定する
+            sprite.position = CGPoint(x: i * sprite.size.width, y: groundTexture.size().height / 2)
+            
+            // スプライトにアクションを設定する
+            sprite.run(repeatScrollGround)
+            
+            // スプライトを追加する
+            scrollNode.addChild(sprite)
+        }
+    }
+    
+    func setupCloud() {
+        // 雲の画像を読み込む
+        let cloudTexture = SKTexture(imageNamed: "cloud")
+        cloudTexture.filteringMode = SKTextureFilteringMode.nearest
+        
+        // 必要な枚数を計算
+        let needCloudNumber = 2.0 + (frame.size.width / cloudTexture.size().width)
+        
+        // スクロールするアクションを作成
+        // 左方向に画像一枚分スクロールさせるアクション
+        let moveCloud = SKAction.moveBy(x: -cloudTexture.size().width , y: 0, duration: 20.0)
+        
+        // 元の位置に戻すアクション
+        let resetCloud = SKAction.moveBy(x: cloudTexture.size().width, y: 0, duration: 0.0)
+        
+        // 左にスクロール->元の位置->左にスクロールと無限に繰り替えるアクション
+        let repeatScrollCloud = SKAction.repeatForever(SKAction.sequence([moveCloud, resetCloud]))
+        
+        // スプライトを配置する
+        stride(from: 0.0, to: needCloudNumber, by: 1.0).forEach { i in
+            let sprite = SKSpriteNode(texture: cloudTexture)
+            sprite.zPosition = -100 // 一番後ろになるようにする
+            
+            // スプライトの表示する位置を指定する
+            sprite.position = CGPoint(x: i * sprite.size.width, y: size.height - cloudTexture.size().height / 2)
+            
+            // スプライトにアニメーションを設定する
+            sprite.run(repeatScrollCloud)
+            
+            // スプライトを追加する
+            scrollNode.addChild(sprite)
+            
+        }
+    }
+    
+    func setupSheep() {
+        
+        
+        // 鳥の画像を2種類読み込む
+        let birdTextureA = SKTexture(imageNamed: "sheep_a")
+        birdTextureA.filteringMode = SKTextureFilteringMode.linear
+        let birdTextureB = SKTexture(imageNamed: "sheep_b")
+        birdTextureB.filteringMode = SKTextureFilteringMode.linear
+        
+        // 2種類のテクスチャを交互に変更するアニメーションを作成
+        let texuresAnimation = SKAction.animate(with: [birdTextureA, birdTextureB], timePerFrame: 0.2)
+        let flap = SKAction.repeatForever(texuresAnimation)
+        
+        // スプライトを作成
+        sheep = SKSpriteNode(texture: birdTextureA)
+        sheep.position = CGPoint(x: 0, y:self.frame.size.height * 0.2)
+        
+        // アニメーションを設定
+        sheep.run(flap)
+        
+        // スプライトを追加する
+        addChild(sheep)
+        
+        //移動のアニメーションを設定
+        let action = SKAction.moveTo(x: 400, duration:3.0)
+        sheep.run(action)
+        
+    }
+
+}
--- SleepSheep/ViewController.swift
+++ SleepSheep/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number:Int = 0
+    var gamescene = GameScene()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // SKViewに型を変換する
+        let skView = self.view as! SKView
+        
+        // ビューと同じサイズでシーンを作成する
+        gamescene = GameScene(size:skView.frame.size)
+        
+        // ビューにシーンを表示する
+        skView.presentScene(gamescene)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+        @IBAction func action() {
+        gamescene.setupSheep()
+        number = number + 1
+        label.text = String(number)
+    }
 }
 
